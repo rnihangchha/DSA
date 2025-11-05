9920712d7989c096186c6f9769867390538ca4b5
rnihangchha <sceptilehero23@gmail.com>
2025-11-05 17:32:56 +0545

    Single Linked List in C

--- single_linklist.c
+++ single_linklist.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+
 /*
 {Feature include}
 create node
@@ -14,18 +16,17 @@ delete at position
 Head-> [data|next]-> [data|next]->NULL
 */
 typedef struct  Node{
-    int data;
+    int16_t data;
     struct Node* next;
 } Node;
 
-Node* createNode(int d){
+Node* createNode(int16_t d){
     Node *ptr = (Node*)malloc(sizeof(Node));
     ptr->data=d;
     ptr->next=NULL;
-    return ptr;
 }
 
-void* insertHead(int d, Node** head){
+void* insertHead(int16_t d, Node** head){
     Node* newNode = createNode(d);
     if(*head == NULL){
         *head = newNode;
@@ -35,7 +36,7 @@ void* insertHead(int d, Node** head){
     *head = newNode;
 }
 
-Node* insertTail(int d, Node** head){
+Node* insertTail(int16_t d, Node** head){
     Node* newNode = createNode(d);
     if(*head == NULL){
         *head = newNode;
@@ -50,7 +51,7 @@ Node* insertTail(int d, Node** head){
     
 }
 
-void insertAtPos(int d, Node** head, int position){
+void insertAtPos(int16_t d, Node** head, int16_t position){
     Node* newNode = createNode(d);
     if(*head == NULL){
         *head = createNode(d);
@@ -64,10 +65,10 @@ void insertAtPos(int d, Node** head, int position){
     }
     else {
         Node* tptr = *head;
-        for(int i=0; tptr != NULL && i < position-1 ; i++ ){
+        for(int16_t i=0; tptr != NULL && i < position-1 ; i++ ){
             tptr = tptr->next;
         }
-        /*If all *next pointer -> next node finished and pointer now assigned to NULL Pointer*/
+        /*If all *next point16_ter -> next node finished and point16_ter now assigned to NULL Point16_ter*/
         if(tptr == NULL) {
             printf("Out of range\n");exit(0);
         }
@@ -110,7 +111,7 @@ void deleteTail(Node **head){
     dptr->next =NULL;
 }
 
-void deleteAtPos(Node **head,int position){
+void deleteAtPos(Node **head,int16_t position){
     Node* dptr = *head;
     if(position==0){
         deleteHeadElement(head);
@@ -120,10 +121,10 @@ void deleteAtPos(Node **head,int position){
         deleteTail(head);
         return;
     }    
-    for(int i=0; dptr != NULL && i<(position-1);i++){
+    for(int16_t i=0; dptr != NULL && i<(position-1);i++){
         dptr = dptr->next; /* 1->2= 2, 3, 4  */
     }
-    if(dptr==NULL || dptr->next==NULL){printf("Out of range");exit(0);}
+    if(dptr==NULL){printf("Out of range");exit(0);}
     Node* next =  dptr->next->next; /*(position+1) = 4 address*/
     free(dptr->next);
     dptr->next = next;
@@ -142,7 +143,7 @@ void print(Node* head){
     printf("\n");
 }
 
-int main(int argc, char ***argv[]){
+int16_t main(int16_t argc, char ***argv[]){
     Node *head = NULL;
     
     insertHead(90,&head);
@@ -158,7 +159,8 @@ int main(int argc, char ***argv[]){
     deleteTail(&head);
     deleteTail(&head);
     print(head);
-    deleteAtPos(&head,2);
+    deleteAtPos(&head,-1);
+    deleteAtPos(&head,-1);
     print(head);
     return 0;
 }
